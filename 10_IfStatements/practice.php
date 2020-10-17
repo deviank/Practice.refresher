9b7c21c076621b7a17758fea918bce4d0a0e4abb
Devian Kapp <forza9882@gmail.com>
2020-10-17 10:30:47 +0200

    If statements.

--- 10_IfStatements/practice.php
+++ 10_IfStatements/practice.php
@@ -1,17 +1,19 @@
 <?php
 	
 	// Constants
+	define("TITLE", "If Statements");
 
 	
 	// Custom Variables
-
+	$my_name = "Devian Kapp";
+	$lesson_num = 10;
 
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>PHP <!-- TITLE --></title>
+		<title>PHP <?php echo TITLE; ?></title>
 		<link href="../assets/styles.css" rel="stylesheet">
 	</head>
 	<body>
@@ -20,7 +22,7 @@
 				<img src="../assets/img/logo.png" alt="PHP">
 			</a>
 			
-			<h1>Tutorial <!-- LESSON NUMBER -->: <small><!-- TITLE --></small></h1>
+			<h1>Tutorial <?php echo $lesson_num; ?>: <small><?php echo TITLE; ?></small></h1>
 			<hr>
 			
 			<h2>Your Example</h2>
@@ -33,7 +35,7 @@
 			
 			<hr>
 			
-			<small>&copy;<!-- YEAR --> - <!-- NAME --></small>
+			<small>&copy;<?php echo date('Y')?> - <?php echo $my_name; ?></small>
 		</div><!-- end wrapper -->
 		
 		<div class="copyright-info">
